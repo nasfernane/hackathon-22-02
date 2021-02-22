6cbe257b91e781e88947b37f73effa606b6e59e1
nasfernane <nasfernane@gmail.com>
2021-02-22 13:43:37 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+require './components/header.php';
+
+$arrayViews = [
+    'home' => 'home.php',
+    'contact' => 'contact.php',
+    'blog' => 'blog.php',
+];
+
+// récupère l'url après "/index/"
+$path = strtolower(substr($_SERVER["REQUEST_URI"], 1));
+
+// si il y a un chemin 
+if ($path) {
+    // si le chemin existe dans les vues
+    if (array_key_exists($path, $arrayViews)) {
+        require("views/$arrayViews[$path]");
+    } else {
+        require('views/404.php');
+    }
+}
+
+require './components/footer.php';
+
+?>
+
+
+
+    
+    
---
+++ views/home.php
@@ -0,0 +1,3 @@
+<main>
+    <h2> Test HOME</h2>
+</main>
