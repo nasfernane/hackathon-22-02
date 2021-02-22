eed788c76b356ffb1e15a86198ee0a239d5b64cd
nasfernane <nasfernane@gmail.com>
2021-02-22 19:24:20 +0100

    ajout troisieme pages et photos figurines

---
+++ views/inscription.php
@@ -0,0 +1,21 @@
+<main class="inscriptionView">
+    <h2>Inscrivez-vous à notre newsletter</h2>
+    <h3>Et gagnez l'une de ces superbes sculptures !</h3>
+
+    <div class="sculptGallery">
+        <img src="/assets/img/sculpt1.jpg" alt="">
+        <img src="/assets/img/sculpt2.jpg" alt="">
+        <img src="/assets/img/sculpt3.jpg" alt="">
+        <img src="/assets/img/sculpt4.jpg" alt="">
+        <img src="/assets/img/sculpt5.jpg" alt="">
+        <img src="/assets/img/sculpt6.jpg" alt="">
+        <img src="/assets/img/sculpt7.jpg" alt="">
+        <img src="/assets/img/sculpt8.jpg" alt="">
+        <img src="/assets/img/sculpt9.jpg" alt="">
+    </div>
+
+    <form class="inscriptionForm">
+        <input id="userEmail" type="email" placeholder="@email">
+        <label for="userEmail">S'abonner à la newsletter</label>
+    </form>
+</main>
--- views/presentation.php
+++ views/presentation.php
@@ -5,26 +5,25 @@
         Ebay accueillera à l'occasion de cette célébration d'anniversaire une vente aux enchères hors du commun : Divers artistes du monde entiers ont été invités à laisser s'exprimer leur créativité en réalisant des oeuvres à partir de pneus usagés ou recyclés. Le bénéfice des ventes sera intégralement reversé à des associations caritatives françaises oeuvrant pour la sécurisation des transports ou encore la prise en charge des victimes d'accidents de la route. Ci-dessous, un premier aperçu de certaines de ces oeuvres
     </p>
 
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-        <img src="..." class="d-block w-100" alt="...">
+    <div id="carouselContainer">
+        <div id="sculptCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/assets/img/bouc2.jpg" class="carouselPicture" alt="Sculpture Bouc">
+                </div>
+                <div class="carousel-item">
+                    <img src="/assets/img/mythos.jpg" class="carouselPicture" alt="Sculpture Mythos">
+                </div>
+                <div class="carousel-item">
+                    <img src="/assets/img/pneus recyclés.jpg" class="carouselPicture" alt="Sculpture Pneus Recyclés">
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
     </div>
 
+    <p>
+        Vous pourrez accéder à des ventes inédites d'objets de collection issus des locaux même de Michelin : Equipement datant des origines de la marque, posters vintages, figurines collector, et plein d'autres. Ce sera aussi l'occasion de découvrir une nouvelle carte routière à tirage limité, qui vous permettra de naviguer plus facilement dans les méandres de la navigation des menus Ebay.
+    </p>
+
+    <img src="/assets/img/banniere.jpg" alt="Bannière Michelin" />
 </main>
