3b6a98077dd548cc1c1378cbcad4679d7fa89e1b
nasfernane <nasfernane@gmail.com>
2021-02-22 15:10:35 +0100

    maj

--- views/home.php
+++ views/home.php
@@ -1,3 +1,34 @@
-<main>
-    <h2> Test HOME</h2>
+<main class="homeView">
+    <h2>La petite histoire de Michelin</h2>
+
+    <article class="homeView__first">
+        <div>
+            <img src="/assets/img/bibendum.jpg" alt="Bibendum">
+            <p>
+            L'entreprise Michelin est fondée en 1889 par les frères André et Édouard Michelin près de Clermont-Ferrand. La réparation des pneus d'un cycliste donne des idées aux frères Michelin qui déposent leur premiers brevets pour des pneus démontables et réparables, ce qui leur construira une certaine notoriété notamment grâce à la course cycliste Paris-Brest-Paris. L'entreprise innove et propose des solutions pneumatiques révolutionnaires et d'une grande praticité pour les cyclistes mais surtout les automobiles qui renforceront la notoriété de la marque par les différentes succès lors de compétitions. La fameuse mascotte de la marque : le « bonhomme bibendum » a été inventé en 1898 par les frères après avoir vu une pile de pneus qui leur rappelaient un bonhomme. Le personnage deviendra officiellement la mascotte de la marque grâce au dessinateur O'Galop. En 1900 est créé le fameux guide Michelin qui propose aux voyageurs des informations pratiques sur les restaurants à travers les différentes routes de France. Au début des années 1900, Michelin étend son usine de Clermont-Ferrand et implante ses premières usines en Europe à Turin et à Londres puis aux États-Unis en 1907. 
+            </p>
+        </div>
+        
+        <p>
+            L'entreprise est l'initiatrice de la sécurité routière en créant les premières plaques de signalisation ainsi que les cartes routières. Le groupe prend part dans la 1ère guerre mondiale notamment par la construction d'avions et le soins de blessés de guerre dans l'enceinte de l'usine de Clermont-Ferrand. Dans les années 1950, Michelin invente le pneu radial qui constitue l'un des éléments phares de la gamme de pneus proposés par la marque. Le succès de ces pneus permettra l'ouverture d'usines en Amérique du Nord. Dès lors, le groupe multiplie les innovations (PAX System, Tweel) et les partenariats avec d'autres marques telle Ferrari.
+
+            Aujourd'hui, Michelin emploie 113 400 personnes et est présente dans quasiment tous les continents du monde. En effet, le groupe est présent dans plus de 170 pays et dispose de 69 sites de production dans 18 pays.
+        </p>
+    </article>
+        
+   
+
+    <h2> Bibendum fête ses 120 ans !</h2>
+
+    <p>
+    A l'occasion de son 120ème anniversaire, Bibendum est fier de vous présenter son nouveau partenariat avec Ebay, la célèbre entreprise de vente pour particuliers, en vue de lancer une série d'évènements festifs et caritatifs.
+
+    Depuis de nombreuses années, Ebay est connu des collectionneurs en tant que plateforme de référence pour dénicher des trouvailles rares ou exotiques. Michelin s'étant vu doté au fil des années d'un héritage iconographique fort constamment alimenté par la créativité d'artistes talentueux à travers le monde, le partenariat entre ces deux grandes entreprises s'est imposé comme une évidence. Vous pourrez à l'occasion de cet évènement retrouver notamment : 
+    • Une série de ventes aux enchères d'artistes qui ont réalisé des sculptures avec des pneus Michelin recyclés. Afin de ponctuer le combat de chaque instant de Michelin pour vous offrir toujours une meilleur qualité et plus de qualité sur vos trajets, les bénéfices seront reversés à diverses associations caritatives françaises oeuvrant pour la sécurisation des transports ou encore la prise en charge des victimes d'accidents de la route.
+    • Un accès exclusif à l'achat d'objets collectors inédits.
+    • Une chance de gagner des cadeaux en vous inscrivant à la newletter hebdomadaire Michelin.
+
+    </p>
+
+
 </main>
